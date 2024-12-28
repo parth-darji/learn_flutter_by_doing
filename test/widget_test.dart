20d56da10257113d99409f54738d527de011d95f
Parth Darji <parthdarji2961@gmail.com>
2024-12-28 16:40:00 +0530

    Project setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:learn_flutter_by_doing/main.dart';
+import 'package:learn_flutter_by_doing/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
